589cf7298c198b0500c153879db84b7b0a8cfbda
Kaitlin Cort <kaitlin.zhang@owasp.org>
2024-04-06 01:47:55 -0600

    minimal Flask Oulipo, no need for Lambda functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,26 @@
-from flask import Flask
-from config import config
-import os
+from flask import Flask, request, render_template
+from v_plus_seven import process_text, load_dictionary
 
-
-# Creates our Flask application.
 app = Flask(__name__)
-config_name = os.getenv('FLASK_CONFIG') or 'default'
-app.config.from_object(config[config_name])
-config[config_name].init_app(app)
+
+# Load your dictionary data once when the app starts
+dictionary_data = load_dictionary('/home/kaizenagility/oulipo/dictionary.json')
 
 
-from app import views, errors
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    transformed_text = ""  # Ensure transformed_text is defined outside the if block
+    if request.method == 'POST':
+        # Extract text from the form submission
+        data = request.form.get('data', '')
+
+        # Process and transform the text
+        processed_text = process_text(data, dictionary_data)
+        transformed_text = processed_text  # Assign the processed text to transformed_text
+
+    # Render the form, passing the transformed text to the template
+    return render_template('form.html', transformed_text=transformed_text)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
